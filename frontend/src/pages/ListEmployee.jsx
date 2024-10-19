8c20a608d4dedce5fcc4c3e7dbfc2a303431b4a6
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-18 21:43:11 -0300

    pages

--- frontend/src/pages/ListEmployee.jsx
+++ frontend/src/pages/ListEmployee.jsx
@@ -1,11 +1,13 @@
 import HeaderTable from "../components/HeaderTable"
 import EmployeeCard from "../components/Employeecard"
+import { Table } from "../styles/Table.style"
 
 export default function ListEmployee(){
     return(
-        <table>   
+        <Table>   
             <HeaderTable/>
             <EmployeeCard/>
-        </table>
+        </Table>
+
     )
 }
