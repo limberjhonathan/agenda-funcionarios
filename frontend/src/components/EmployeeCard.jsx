64f455819a2321457d49edb002557f682dbac669
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-21 22:05:04 -0300

    ajustes no card

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button"
+
 export default function EmployeeCard() {
     return (
         <tbody>
@@ -6,18 +8,21 @@ export default function EmployeeCard() {
                 <td>Desenvolvedor</td>
                 <td>pessoa@gmail.com</td>
                 <td>R$ 5.000</td>
+                <td><Button/></td>
             </tr>
             <tr>
                 <td>Roberto</td>
                 <td>Desenvolvedor</td>
                 <td>pessoa@gmail.com</td>
                 <td>R$ 5.000</td>
+                <td><Button/></td>
             </tr>
             <tr>
                 <td>Roberto</td>
                 <td>Desenvolvedor</td>
                 <td>pessoa@gmail.com</td>
                 <td>R$ 5.000</td>
+                <td><Button/></td>
             </tr>
         </tbody>
         
