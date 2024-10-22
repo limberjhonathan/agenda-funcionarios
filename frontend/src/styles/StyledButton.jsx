a481a177e3821cebf4c2903074a96ea2cf62443d
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-21 22:07:05 -0300

    estilo button

---
+++ frontend/src/styles/StyledButton.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const StyledButton = styled.button`
+    background-color: #eece42;
+    color: #6c6969;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 1.4px;
+`
