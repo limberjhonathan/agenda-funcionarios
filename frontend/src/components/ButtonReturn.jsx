cb181c7a8f11adaa269f2587b6518cf3ef9b6483
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-22 23:24:27 -0300

    button

---
+++ frontend/src/components/ButtonReturn.jsx
@@ -0,0 +1,10 @@
+import { IoReturnDownBackSharp } from "react-icons/io5";
+import { ButtonReturnStyle } from "../styles/ButtonReturnStyle";
+
+export default function ButtonReturn(){
+    return(
+        <ButtonReturnStyle>
+            <IoReturnDownBackSharp/>
+        </ButtonReturnStyle>
+    )
+}
