db5a184035a6b486cb1f214872b172bcf4931fe1
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-22 23:25:36 -0300

    card

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -24,6 +24,27 @@ export default function EmployeeCard() {
                 <td>R$ 5.000</td>
                 <td><Button/></td>
             </tr>
+            <tr>
+                <td>Roberto</td>
+                <td>Desenvolvedor</td>
+                <td>pessoa@gmail.com</td>
+                <td>R$ 5.000</td>
+                <td><Button/></td>
+            </tr>
+            <tr>
+                <td>Roberto</td>
+                <td>Desenvolvedor</td>
+                <td>pessoa@gmail.com</td>
+                <td>R$ 5.000</td>
+                <td><Button/></td>
+            </tr>
+            <tr>
+                <td>Roberto</td>
+                <td>Desenvolvedor</td>
+                <td>pessoa@gmail.com</td>
+                <td>R$ 5.000</td>
+                <td><Button/></td>
+            </tr>
         </tbody>
         
     )
