d501bf03a6a8aeb88e1390fd7e305f9b6be449a6
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-22 23:26:36 -0300

    add button return

--- frontend/src/pages/ListEmployee.jsx
+++ frontend/src/pages/ListEmployee.jsx
@@ -1,13 +1,17 @@
 import HeaderTable from "../components/HeaderTable"
 import EmployeeCard from "../components/Employeecard"
+import ButtonReturn from "../components/BUttonReturn"
 import { Table } from "../styles/Table.style"
 
 export default function ListEmployee(){
     return(
-        <Table>   
-            <HeaderTable/>
-            <EmployeeCard/>
-        </Table>
+        <div>
+            <ButtonReturn/>
+            <Table>   
+                <HeaderTable/>
+                <EmployeeCard/>
+            </Table>
+        </div>
 
     )
 }
