feae7af8ac2a4e60da89aa71f280579c6533a906
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-28 23:10:05 -0300

    navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import iconFunc from '../assets/icon-func.png';
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar(){
+    return(
+        <nav>
+            <Container>
+                <Link className={styles.container_link} to={'/'}>
+                    <img src={iconFunc} alt="" />
+                </Link>
+                <ul className={styles.list}>
+                    <li>
+                        <Link to={"/list"}>Funcionarios</Link>
+                    </li>
+                    <li>
+                        <Link>Adicionar</Link>
+                    </li>
+                    <li>
+                        <Link>Contato</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
