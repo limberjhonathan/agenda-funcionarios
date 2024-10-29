9dd02cd2c1269c7bdcbdd62a58e19f8179a76466
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-28 23:17:03 -0300

    global style

--- frontend/src/styles/GlobalStyles.jsx
+++ frontend/src/styles/GlobalStyles.jsx
@@ -5,11 +5,16 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        user-select: none; 
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        font-family: sans-serif;
     }
 
     body{
         width: 100%;
         height: 100%;
-        background-color: #1b313e;
+        background-color: #153446;
     }
 `
