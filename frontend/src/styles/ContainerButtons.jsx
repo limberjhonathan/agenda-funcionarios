4ec28e5c31df7ac2828b2439996477d7cfafa9b3
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-28 23:16:38 -0300

    Conatiner button

---
+++ frontend/src/styles/ContainerButtons.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContainerButtons = styled.div`
+        position: relative;
+        left: 0;
+        top: 4em;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+`
