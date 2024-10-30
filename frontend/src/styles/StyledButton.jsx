b36acd70910c54ff66b757326af9743fd5406488
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-29 23:41:15 -0300

    style buttom

--- frontend/src/styles/StyledButton.jsx
+++ frontend/src/styles/StyledButton.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components"
 
 export const StyledButton = styled.button`
-    background-color: #eece42;
+    background-color: #f8dd65;
     color: #6c6969;
-    padding: 10px 20px;
+    padding: 10px 30px;
     border: none;
     border-radius: 20px;
     cursor: pointer;
@@ -11,4 +11,5 @@ export const StyledButton = styled.button`
     text-transform: uppercase;
     font-weight: bold;
     letter-spacing: 1.4px;
+    outline: 2px solid #b6b6b6;
 `
