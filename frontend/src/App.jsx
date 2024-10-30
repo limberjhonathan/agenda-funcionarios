bab4ccdfbe8816c900ab2545211897bdd34cec84
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-29 23:44:40 -0300

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-import Routes from './router'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ListEmployee from './pages/ListEmployee';
+import Home from './pages/Home';
+import EditEmployee from './pages/EditEmployee';
+import Navbar from './components/NavBar';
+import Footer from './components/Fotter';
+import AddEmployee from './pages/AddEmployee';
 
 function App() {
   return ( 
-    <Routes/>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+          <Route Component= { ListEmployee } path='/list' exact/>
+          <Route Component= { EditEmployee } path='/edit' exact/>
+          <Route Component= { Home } path='/' exact/>
+          <Route Component= { AddEmployee } path='/addemployee' exact/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
